afadd98210cd3e9a11100f5a8936abc13c4b832c
Robby Ross <>
2021-05-18 01:36:38 +0000

    added C to fuel temp

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ console.log("\n"+dashes+"\n> ASTRONAUT INFO \n"+dashes);
 console.log("* Count: "+astronautCount+"\n* Status: "+astronautStatus);
 
 console.log("\n"+dashes+"\n> FUEL DATA \n"+dashes); 
-console.log("* Fuel temp celsius: "+fuelTempCelsius+"\n* Fuel level: "+fuelLevel);
+console.log("* Fuel temp celsius: "+fuelTempCelsius+" C\n* Fuel level: "+fuelLevel);
 
 console.log("\n"+dashes+"\n> MASS DATA \n"+dashes);
 console.log("* Crew mass: "+crewMassKg+" kg\n* Fuel mass: "+fuelMassKg+" kg\n* Shuttle mass: "+shuttleMassKg+" kg\n* Total mass: "+totalMassKg+" kg");
@@ -38,40 +38,3 @@ console.log("* Clear for takeoff: YES");
 
 
 // BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
-/*
--------------------------------------
-> LC04 - LAUNCH CHECKLIST
--------------------------------------
-Date: Monday 2019-03-18
-Time: 10:05:34 AM
-
--------------------------------------
-> ASTRONAUT INFO
--------------------------------------
-* count: 7
-* status: ready
-
--------------------------------------
-> FUEL DATA
--------------------------------------
-* Fuel temp celsius: -225 C
-* Fuel level: 100%
-
--------------------------------------
-> MASS DATA
--------------------------------------
-* Crew mass: 564.9 kg
-* Fuel mass: 760000 kg
-* Shuttle mass: 74842.31 kg
-* Total mass: 835407.21 kg
-
--------------------------------------
-> FLIGHT PLAN
--------------------------------------
-* weather: clear
-
--------------------------------------
-> OVERALL STATUS
--------------------------------------
-* Clear for takeoff: YES
-/*
